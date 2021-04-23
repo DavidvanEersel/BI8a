a746c0de8f7d0713c2f35a9b545c27d095453857
DHildebrand <dominicmrbrown@gmailcom>
2021-04-23 11:58:48 +0200

    initial commit

---
+++ doewatleuks/gtgp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from utils import test
+
+def index(request):
+    print(test.main())
+    return render(request, 'gtgp/home.html')
+
+def upload(request):
+    return render(request, 'gtgp/upload.html')
+
+# Create your views here.
