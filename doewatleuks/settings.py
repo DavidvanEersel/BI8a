5e9bfef43b3dc72c5d8eede21718e6e4376c707e
daaf <davidvaneersel@live.nl>
2021-05-10 11:46:02 +0200

    fixed css

--- doewatleuks/settings.py
+++ doewatleuks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
 STATIC_URL = '/static/'
 
 # Default primary key field type
