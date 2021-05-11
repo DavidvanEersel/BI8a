54ff5d4bd8e14dc811afa4bae8493e17580956f1
bvvbin <BLM.vanVelzen@student.han.nl>
2021-05-11 15:43:06 +0200

    Update requirements & functions

--- utils/functions.py
+++ utils/functions.py
@@ -1,27 +1,56 @@
 from Bio import Entrez
+import requests
 
-def entrezSearch():
-    idList = []
 
-    Entrez.email = "email"    #Misschien gebruiker om een email vragen? 
-    searchFrom = '2020/06/12' #Must be YYYY/MM/DD OF YYYY/MM OF YYYY
-    term='orchid'             #In Teams staat een advanced query voor een zoekterm
+def entrezSearch(keywords="orchid",
+                 genenames=["ABC", "ESR1"],
+                 search_from="2020/06/12",
+                 ):
+    Entrez.email = "email"  # Misschien gebruiker om een email vragen?
+
+    searchFrom = '2020/06/12'  # Must be YYYY/MM/DD OF YYYY/MM OF YYYY
+    term = 'orchid'  # In Teams staat een advanced query voor een zoekterm
     handle = Entrez.esearch(db="pubmed", term=term, mindate=searchFrom)
-    record = Entrez.read(handle)
+    res = Entrez.read(handle)
     handle.close()
-    for id in record:
-        print(id['idList'])
-    #TODO Maak hier een lijst van zodat je een lijst met PubMed ID's hebt voor een pubtator search
 
-    return idList
+    # https://www.ncbi.nlm.nih.gov/dbvar/content/tools/entrez/
+    list_ids = res['IdList']
 
-def pubtatorSearch(idList):
-    #TODO Maak vind een manier om met de NCBI link te werken
-    #Voorbeelden pubtator search:
+    return list_ids
+
+
+def pubtatorSearch(list_ids):
+    # TODO Maak vind een manier om met de NCBI link te werken
+    # Voorbeelden pubtator search:
     # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids=28483577&concepts=gene
     # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocxml?pmcids=PMC6207735
     # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmids=28483577,28483578,28483579
     # Leren format pubtator format lezen
 
+    # Defaulting to gene, disease and mutation
+    base_url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids={}&concepts=gene,disease,mutation"
+
+    # Format IDs for PubTator
+    string_ids = ""
+    for pmcid in list_ids:
+        string_ids += pmcid + ','
+
+    # Remove last comma delimiter
+    string_ids = string_ids[:-1]
+    format_url = base_url.format(string_ids)
+
+    # Get page details, var.text is important here
+    res = requests.get(format_url)
+
+    text = res.text.split("\n")
+    for line in text:
+        if line:
+            print("Line")
+        else:
+            print("Next")
+
     return None
-    
+
+
+pubtatorSearch(entrezSearch())
