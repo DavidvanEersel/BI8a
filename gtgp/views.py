7306257ae771d4f400eccd8981259d9763d73b68
daaf <davidvaneersel@live.nl>
2021-05-11 15:09:07 +0200

    parameters kunnen worden opgehaald. daarbij worden ze in een dictionary gezet

--- gtgp/views.py
+++ gtgp/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
 
-from utils import test
+
 
 def index(request):
-    print(test.main())
-    return render(request, 'gtgp/home.html')
+    if request.method == 'POST':
+        text_keywords = request.POST.get('text_keywords')
+        text_gene_name = request.POST.get('text_gene_name')
+        date_after = request.POST.get('date_after')
+        text_exclude_genepanel = request.POST.get('text_exclude_genepanel')
+        # TODO gene_name = none dan zoek je naar NIEUWE GENEN
+        # TODO gene_name != none dan zoek je naar NIEUWE INFO OVER DE GENEN
+        parameter = {}
+        if text_gene_name is not None:
+            parameter = {"keywords": text_keywords,
+                         "gene_name": text_gene_name,
+                         "date_after": date_after,
+                         "exclude_genepanel": text_exclude_genepanel}
+        elif text_gene_name is None:
+            parameter = {"keywords": text_keywords,
+                         "date_after": date_after,
+                         "exclude_genepanel": text_exclude_genepanel}
+
+            return render(request, 'gtgp/home.html', {'results': results})
+
+        else:
+            return render(request, 'gtgp/home.html', {'results': results})
+    else:
+        return render(request, 'gtgp/home.html', {'results': results})
+
 
 def upload(request):
     return render(request, 'gtgp/upload.html')
 
+
 def manual(request):
     return render(request, 'gtgp/manual.html')
+
 
 def about(request):
     return render(request, 'gtgp/about.html')
