05e7016d51ab9d01ea6225802d852ea249f14e12
DHildebrand <dominicmrbrown@gmailcom>
2021-05-17 22:32:58 +0200

    worked on pubtatorSearch, needs debugging

--- utils/functions.py
+++ utils/functions.py
@@ -7,12 +7,17 @@ def entrezSearch(parameter):
     Input:  string keywords(str(term1 AND/OR term2))
             list genenames[gene1, gene2]
             string search_from(str date example(2020/06/12))
-    Return: list[pmid's]"""
+    Return: list[pmid's]
+    OR 
+    Return: None if input is empty or null"""
     # Must be YYYY/MM/DD OF YYYY/MM OF YYYY
     # In Teams staat een advanced query voor een zoekterm
-    # Moeten een manier zien te maken om AND / OR af anders te maken uit de website, anders
     # Misschien gebruiker om een email vragen?
     # https://www.ncbi.nlm.nih.gov/dbvar/content/tools/entrez/
+    
+    if parameter == "" or parameter == None:
+        return None
+
     keywords = parameter["keywords"]
     search_from = parameter["date_after"]
     query = ""
@@ -48,11 +53,14 @@ def entrezSearch(parameter):
 
 
 def pubtatorSearch(list_ids):
-    # TODO Maak vind een manier om met de NCBI link te werken
-    # Voorbeelden pubtator search:
-    # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids=28483577&concepts=gene
-    # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocxml?pmcids=PMC6207735
-    # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmids=28483577,28483578,28483579
+    """Function uses PubTator API to textmine found hits. Hits get rudimentary score.
+    Input:  list[str(pmid), str(pmid)]
+    Return: Dict{key(str(pmid)) : tuple(str(score), str(pubtator_link), gennames[gene], diseases[disease], mutations[mutation])) Lists may be empty.
+    OR
+    Return: None if input is empty or null"""
+    
+    if list_ids == '' or list_ids == None:
+        return None
 
     # Defaulting to gene, disease and mutation
     base_url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids={}&concepts=gene,disease,mutation"
@@ -69,33 +77,72 @@ def pubtatorSearch(list_ids):
     # Get page details, var.text is important here
     res = requests.get(format_url)
 
+    # Scores: Gene, mutation or disease in title:   +5 pts
+    # Scores: Mutation or disease in abstract:      +2 pts
+    # Scores: Gene in abstract                      +1 pt
+    titleLine = ""
+    scored = False
+    articleLink = "https://www.ncbi.nlm.nih.gov/research/pubtator/?view=docsum&query=article"
+    articleScore = 0
+    gennames = []
+    diseases = []
+    mutations = []
+    returnDict = {}
     text = res.text.split("\n")
+    
     for line in text:
-        if line:
-            print("Line")
-        else:
-            print("Next")
 
-    # Defaulting to gene, disease and mutation
-    base_url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids={}&concepts=gene,disease,mutation"
+        if '|t|' in line:
+            titleLine = line.split('|t|')
+            pmid = titleLine[0]
+            title = titleLine[1]
+            continue
+        
+        if '|a|' in line:
+            continue
 
-    # Format IDs for PubTator
-    string_ids = ""
-    for pmcid in list_ids:
-        string_ids += pmcid + ','
+        
+        if line != "":
+            # print(line.split("\t"))
+            terms = line.split("\t")
+            if int(terms[1]) < len(title):
+                articleScore += 5
+                scored = True
+            if terms[4] == "Disease":
+                diseases = checkList(terms[3], diseases)
+                if scored == False:
+                    articleScore += 2
+            elif terms[4] == "Mutation":
+                mutations = checkList(terms[3], mutations)
+                if scored == False: 
+                    articleScore += 2
+            elif terms[4] == "Gene":
+                gennames = checkList(terms[3], gennames)
+                if scored == False: 
+                    articleScore += 1
+            else:
+                print("A unexpected scoring error has occured for: "+ terms[4])
 
-    # Remove last comma delimiter
-    string_ids = string_ids[:-1]
-    format_url = base_url.format(string_ids)
+        if line == "":
+            
+            # Dict{key(str(pmid)) : tuple(str(articleScore), str(articleLink), gennames[gene], diseases[disease], mutations[mutation])) Lists may be empty.
+            articleLink = articleLink.replace("article", pmid)        
+            valueTuple = (str(articleScore), articleLink, gennames, diseases, mutations)
+            returnDict[pmid] = valueTuple
+            titleLine = ""
+            scored = False
+            articleLink = "https://www.ncbi.nlm.nih.gov/research/pubtator/?view=docsum&query=article"
+            articleScore = 0
+            gennames = []
+            diseases = []
+            mutations = []
 
-    # Get page details, var.text is important here
-    res = requests.get(format_url)
+    return returnDict
 
-    text = res.text.split("\n")
-    for line in text:
-        if line:
-            print("Line")
-        else:
-            print("Next")
-
-    return None
+def checkList(var, varList):
+    """Checks if variable is in list, if not, appends
+    Input: str(var), varlist[var]
+    Return: varlist[]"""
+    if var not in varList:
+        varList.append(var)
+    return varList
