cdc7d29411f915d857c90c5b33e7d86e24ade91d
DHildebrand <dominicmrbrown@gmailcom>
2021-05-17 20:11:11 +0200

    starting work on pubtatorSearch

--- utils/functions.py
+++ utils/functions.py
@@ -6,9 +6,6 @@ def entrezSearch(keywords="orchid",
                  genenames=["ABC", "ESR1"],
                  searchFrom="2020/06/12",
                  ):
-   
-    boolAnd = True                                 # Misschien een boolean gebruiken voor de AND / OR statement? Anders opbouwen?
-
     """Function searches PubMed for articles related to a search
     Input:  string keywords(str(term1 AND/OR term2))
             list genenames[gene1, gene2]
@@ -19,10 +16,7 @@ def entrezSearch(keywords="orchid",
     searchFrom = '2020/06/12'                      # Must be YYYY/MM/DD OF YYYY/MM OF YYYY
     term = 'orchid'                                # In Teams staat een advanced query voor een zoekterm
 
-    # Moeten een manier zien te maken om AND / OR af anders te maken uit de website, anders
-    if boolAnd == True:
-        term = keywords
-    if genenames != "":                            # Om af te vangen dat er geen 
+    if genenames != "":                            # Om errors af te vangen als er niks is ingevoerd. 
         for gene in genenames:
             keywords += " AND " + gene
     if searchFrom == "":
@@ -36,7 +30,7 @@ def entrezSearch(keywords="orchid",
     searchFrom = '2020/06/12' #Must be YYYY/MM/DD OF YYYY/MM OF YYYY
     term='orchid'             #In Teams staat een advanced query voor een zoekterm
     handle = Entrez.esearch(db="pubmed", term=term, mindate=searchFrom)
-    record = Entrez.read(handle)
+    res = Entrez.read(handle)
     handle.close()
 
     # https://www.ncbi.nlm.nih.gov/dbvar/content/tools/entrez/
@@ -51,6 +45,28 @@ def pubtatorSearch(list_ids):
     # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids=28483577&concepts=gene
     # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocxml?pmcids=PMC6207735
     # https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmids=28483577,28483578,28483579
+
+    # Defaulting to gene, disease and mutation
+    base_url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids={}&concepts=gene,disease,mutation"
+
+    # Format IDs for PubTator
+    string_ids = ""
+    for pmcid in list_ids:
+        string_ids += pmcid + ','
+
+    # Remove last comma delimiter
+    string_ids = string_ids[:-1]
+    format_url = base_url.format(string_ids)
+
+    # Get page details, var.text is important here
+    res = requests.get(format_url)
+
+    text = res.text.split("\n")
+    for line in text:
+        if line:
+            print("Line")
+        else:
+            print("Next")
 
     # Defaulting to gene, disease and mutation
     base_url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids={}&concepts=gene,disease,mutation"
