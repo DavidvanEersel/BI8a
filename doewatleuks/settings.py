4457e9398c517601bebdd673bb0e7e6910753b26
DHildebrand <dominicmrbrown@gmailcom>
2021-06-04 10:34:51 +0200

    worked on HTML files not finished

--- doewatleuks/settings.py
+++ doewatleuks/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'doewatleuks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+        os.path.join(BASE_DIR, "gtgp", "templates", "gtgp")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
